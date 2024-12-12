6e32e2c5e14353a621bd138fed353130c7361ee1
Tae young Kim <tyk0707@gmail.com>
2024-12-12 14:50:56 +0900

    1500

---
+++ section07/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", //src 폴더 내의 모든 jsx 파일에 적용 
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
