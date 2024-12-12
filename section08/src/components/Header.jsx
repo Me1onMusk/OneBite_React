efc56d8fba3de18a6bbad467613b7167652d5efc
Tae young Kim <tyk0707@gmail.com>
2024-12-12 20:23:39 +0900

    2030

---
+++ section08/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+
+const Header = () => {
+
+    let t = new Date();
+    const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    const day = days[t.getDay()];
+
+    return (
+        <div className="Header">
+            <h3>오늘은 📆</h3>
+            <h1>{ t.getFullYear()+"년 " + (t.getMonth()+1)+"월 " + t.getDay()+"일 " + day }</h1>
+        </div>
+    );
+};
+
+export default Header;
